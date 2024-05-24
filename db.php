027fdbb577f2ad7be059cad1657d0bc1e4349983
th1enlm02 <21521460@gm.uit.edu.vn>
2024-05-25 02:09:51 +0700

    deploy to local successfully

--- db.php
+++ db.php
@@ -1,8 +1,15 @@
 <?php
-$host = "localhost";
-$dbname = "class_service";
-$user = "postgres";
-$password = "root";
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['HOST'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['USER'];
+$password = $_ENV['PASSWORD'];
 
 $conn = pg_connect("host=$host dbname=$dbname user=$user password=$password");
 
